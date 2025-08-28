1e06d0fcd1b5a8abcc0a29a6725dabf96bb7bb37
Onkar Shitole <onkarshitole89@gmail.com>
2025-08-28 23:37:53 +0530

    Create main.py
    bsefjnjdf

---
+++ main.py
@@ -0,0 +1,11 @@
+def hello_gcs(event, context):
+    """
+    Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file_name = event['name']
+    bucket_name = event['bucket']
+
+    print(f"File {file_name} uploaded to bucket {bucket_name}.")
